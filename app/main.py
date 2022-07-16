25d1626299223cf4a87c8e0eb1c2c2f3280eb1bf
bernie-graves <75196548+bernie-graves@users.noreply.github.com>
2022-07-16 18:28:52 +0800

    built api

---
+++ app/main.py
@@ -0,0 +1,45 @@
+import uvicorn
+from typing import List
+from pydantic import BaseModel
+from fastapi import FastAPI
+from tensorflow.keras.models import load_model
+
+
+app = FastAPI()
+
+model = load_model('../sentiment_model')
+
+
+class Input(BaseModel):
+    text: str
+
+
+@app.get('/')
+def index():
+    return {'message': 'Welcome to the Sentiment Analysis API'}
+
+
+@app.post('/predict')
+def predict_sentiment(data: Input):
+    """ FastAPI 
+    Args:
+        data: json file 
+    Returns:
+        sentiment: Sentiment of the text - (Negative or Positive)
+        prediction: probability of text being positive
+    """
+    data = data.dict()
+    review = data['text']
+    prediction = model.predict([review])
+    prediction_prob = prediction.tolist()[0][0]
+
+    pred_map = {0: 'Negative', 1: 'Positive'}
+
+    return {
+        'sentiment': pred_map[round(prediction_prob)],
+        'probability': prediction_prob
+    }
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)
